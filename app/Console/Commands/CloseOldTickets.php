f0b04b864417dd4b02b6fa4c2c8a7b4f5495cfaa
ilnur17021992 <ilnur17021992@mail.ru>
2023-02-06 18:51:53 +0300

    auto close processing tickets after N minutes

---
+++ app/Console/Commands/CloseOldTickets.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\Ticket;
+use App\Services\TicketService;
+use Illuminate\Console\Command;
+
+class CloseOldTickets extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tickets:close';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Close old tickets';
+
+    /**
+     * The number of minutes to consider a ticket as old.
+     *
+     * @var int
+     */
+    const MINUTES_OLD = 1;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $ticketService = new TicketService();
+        $tenMinutesAgo = Carbon::now()->subMinutes(self::MINUTES_OLD);
+
+        $tickets = Ticket::where('status', 'Processing')
+            ->where('updated_at', '<', $tenMinutesAgo)
+            ->get();
+
+        foreach ($tickets as $ticket) {
+            $ticketService->close($ticket->id);
+        }
+
+        $this->info(sprintf('Closed %s tickets', $tickets->count()));
+
+        return Command::SUCCESS;
+    }
+}
